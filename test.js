24c28757dc51bdfbbe393e1ae5c8d5f6b4504842
Devin Chen <d3vin.chen@gmail.com>
2019-09-18 01:01:09 +0800

    bluebird .all()

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 import test from "ava";
 import Promise from "bluebird";
+import { getDirFilesContent, getDirFiles } from "./helpers.js";
 
 test("foo", t => {
   t.pass();
@@ -8,4 +9,13 @@ test("foo", t => {
 test("bar", async t => {
   const bar = Promise.delay(1000).then(() => "bar");
   t.is(await bar, "bar");
+});
+
+test("getDirFiles()", async t => {
+  t.deepEqual(await getDirFiles(), ["foo.txt", "bar.txt"]);
+});
+
+test("getDirFilesContent()", async t => {
+  const bar = getDirFilesContent();
+  t.is(await bar, "foo 200ms\nbar 100ms");
 });
